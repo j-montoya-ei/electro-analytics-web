6c7a458dabb787cdad1f2a1f1e392ef766e3ecaa
j-montoya-ei <j.montoya@ei.com.co>
2026-08-20 16:28:31 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const routeTitles: Record<string, string> = {
   '/empleados': 'Empleados',
   '/asistencia': 'Asistencia',
   '/inasistencias': 'Inasistencias',
-  '/asistencia/administrativos': 'Puntualidad',
+  '/llegadas-tarde': 'Llegadas tarde',
 }
 
 export default function Header({ userEmail }: { userEmail: string | undefined }) {
