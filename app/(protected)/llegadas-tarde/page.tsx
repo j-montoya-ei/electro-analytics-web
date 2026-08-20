7c06ff4a310bae4a7459941d581aa3058ad70af6
j-montoya-ei <j.montoya@ei.com.co>
2026-08-20 16:07:10 -0500

    Update page.tsx

--- app/(protected)/llegadas-tarde/page.tsx
+++ app/(protected)/llegadas-tarde/page.tsx
@@ -72,6 +72,7 @@ export default async function LlegadasTardePage({
         </p>
       </div>
 
-    <LlegadasTardeTable data={filas} />
+      <LlegadasTardeTable data={filas} />
+    </div>
   )
 }
